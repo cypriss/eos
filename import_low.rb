1f1240d16ae67a6b6d270bb940042a7e001d5cd8
Jonathan Novak <jnovak@gmail.com>
2012-04-27 17:01:15 -0700

    Lower threshold

--- import_low.rb
+++ import_low.rb
@@ -4,14 +4,15 @@ require 'eos'
 
 eos = Eos.load
 s = eos.stores.select {|z| z.name == "Sports Store" }.first
-s.import_low(10000, 500, 50_000)
+s.import_low(1000, 500, 50_000)
 
 s = eos.stores.select {|z| z.name == "Hardware Store" }.first
-s.import_low(10000, 500, 50_000) if s
+s.import_low(1000, 500, 50_000) if s
 
 s = eos.stores.select {|z| z.name == "Apparel Store" }.first
-s.import_low(10000, 500, 50_000) if s
+s.import_low(1000, 500, 50_000) if s
 
 s = eos.stores.select {|z| z.name == "Electronics Store" }.first
-s.import_low(10000, 500, 50_000) if s
+s.import_low(1000, 500, 50_000) if s
 
+puts `git pull`
